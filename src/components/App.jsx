d7b3df4a354dc9a95813272802e9d9d8f916ee79
Hanna Tubolets <116286387+HannaTubolets@users.noreply.github.com>
2023-02-14 16:21:49 +0300

    Added saving to Local Storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,16 @@
 import { Component } from 'react';
+import storage from 'helpers/storage';
 import { nanoid } from 'nanoid';
 import { ContactList } from './ContactList/ContactList';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
+
+const CONTACTS = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
 export class App extends Component {
   state = {
@@ -14,6 +22,19 @@ export class App extends Component {
     ],
     filter: '',
   };
+
+  componentDidMount() {
+    const savedContacts = storage.load('contactsToLS') ?? CONTACTS;
+    this.setState({ contacts: savedContacts });
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { contacts } = this.state;
+    if (prevState.contacts !== this.state.contacts) {
+      storage.save('contactsToLS', contacts);
+      console.log(contacts);
+    }
+  }
 
   onAddContact = contact => {
     if (this.state.contacts.some(cont => cont.name === contact.name)) {
